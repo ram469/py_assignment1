0944a437b74e9a2b7f486ca61ab8c09967907140
ramm <ram.physics69@gmail.com>
2019-01-15 00:04:26 +0530

    Python first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# Python is Easy Assignment 1
+
+# Name of the song
+SongName = "Our God"
+
+# Singer name
+Singer = "Chris Tomlin"
+
+# Album name
+Album = "If our God is for us"
+
+# Genre of the song
+Genre = "Worship"
+
+# Releasing date
+DateReleased = 2010
+
+# Track length
+Length = 285
+
+# Lyric writers
+LyricWriters = "Chris Tomlin, Jesse Reeves, Ed Cash, Jonas Myrin"
+
+print("Song Name: ", SongName)
+print("Singer NMae: ", Singer)
+print("Album Name:", Album)
+print("Genre: ", Genre)
+print("Release Date: ", DateReleased)
+print("Track Length: ", Length, "Sec")
+print("Lyrics Writers: ", LyricWriters)
